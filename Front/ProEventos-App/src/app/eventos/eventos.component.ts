0c5898d09b6ed5f546b7ab3763bd7ebb3bf1a698
fredcoutinho52 <fredcoutinho52@gmail.com>
2023-03-30 09:51:10 -0300

    added table

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  showImage: boolean = true;
+  private _search: string = "";
+
+  public get search(): string {
+    return this._search;
+  }
+
+  public set search(value: string) {
+    this._search = value;
+    this.eventosFiltrados = this.search ? this.filterEvents(this.search) : this.eventos;
+  }
 
   constructor(
     private http: HttpClient,
@@ -18,9 +30,23 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  filterEvents(filterSearch: string): any {
+    filterSearch = filterSearch.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filterSearch) !== -1
+    )
+  }
+
+  setShowImage(): void {
+    this.showImage = !this.showImage;
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = response;
+      },
       error => console.log(error)
     );
   }
